b23624fd4d8eb7ecc92b8421db14c834934f7c40
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-07 16:15:10 -0300

    change theme começo

--- src/app/components/changeTheme.js
+++ src/app/components/changeTheme.js
@@ -1,13 +1,33 @@
-import { useState } from "react"
+'use client'
+import { useState, useEffect } from "react";
 
-changeTheme = () =>{
-    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
+const ThemeToggle = () => {
+  // Estado para armazenar o tema atual
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
-    changeValueTheme = () =>{
-        setTheme(theme == 'light' ? 'dark' : 'light');
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    if (theme === "dark") {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
     }
-    <button onClick={changeValueTheme}>
-        {theme == 'light' ? }
+  }, [theme]);
+
+  const changeValueTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+    console.log(theme)
+  };
+
+  return (
+    <button onClick={changeValueTheme} class="p-2 rounded-full bg-light-card dark:bg-dark-card transition">
+      <img
+        src={theme === "light" ? "/assets/darkTheme.png" : "/assets/light.png"}
+        alt={theme === "light" ? "Modo Escuro" : "Modo Claro"}
+        className={theme === "light" ? "w-6 h-6 p-0" : " w-8 h-8 p-0"}
+      />
     </button>
-}
-export default changeTheme
+  );
+};
+
+export default ThemeToggle;
