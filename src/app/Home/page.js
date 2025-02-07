848d53ef97bb022f1b25176ac72685e4f0d2d087
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-06 22:27:53 -0300

    iniciando front

---
+++ src/app/Home/page.js
@@ -0,0 +1,16 @@
+const Page = () => {
+    return (
+        <>
+            {/* Construindo interface inicial */}
+            <div className="h-1/2 w-1/4">
+                
+            </div>
+            <div className="h-1/2 w-3/4">
+            <h1 className="h-3">Estou aqui</h1>
+            
+            </div>
+        </>
+    );
+}
+
+export default Page;
