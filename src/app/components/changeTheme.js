551d1692b06fb8f0976edd9ffa69dd20be876a4d
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-12 22:35:53 -0300

    theme bottom mudanças

--- src/app/components/changeTheme.js
+++ src/app/components/changeTheme.js
@@ -2,29 +2,36 @@
 import { useState, useEffect } from "react";
 
 const ThemeToggle = () => {
-  // Estado para armazenar o tema atual
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  // Estado para armazenar o tema atual, pegando do localStorage ou usando 'light' como padrão
+  const [theme, setTheme] = useState(() => {
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem("theme" || "light");
+    }
+    return "light";
+  });
 
   useEffect(() => {
-    localStorage.setItem("theme", theme);
-    if (theme === "dark") {
-      document.body.classList.add("dark");
-    } else {
-      document.body.classList.remove("dark");
+    if (typeof window !== 'undefined') {
+      localStorage.setItem("theme", theme);
+      if (theme === "dark") {
+        document.body.classList.add("dark");
+      } else {
+        document.body.classList.remove("dark");
+      }
     }
   }, [theme]);
 
   const changeValueTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-    console.log(theme)
+    setTheme (theme === "light" ? "dark" : "light");
   };
 
   return (
-    <button onClick={changeValueTheme} class="p-2 rounded-full bg-light-card dark:bg-dark-card transition">
+    <button onClick={changeValueTheme} class="p-2 rounded-full bg-light-card transition border border-dark-background">
       <img
         src={theme === "light" ? "/assets/darkTheme.png" : "/assets/light.png"}
         alt={theme === "light" ? "Modo Escuro" : "Modo Claro"}
-        className={theme === "light" ? "w-6 h-6 p-0" : " w-8 h-8 p-0"}
+        className={`w-6 h-6 p-0 ${theme === "light" ? "" : ""}`}
+
       />
     </button>
   );
