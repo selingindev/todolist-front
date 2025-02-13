7b47bcc9238813ba9a1737e8a480c321e43e0b1c
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-12 22:36:09 -0300

    sintaxe

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -5,11 +5,11 @@ const Page = () => {
         <>
             {/* Construindo interface inicial */}
             <div className="h-1/2 w-1/4">
-                
+
             </div>
             <div className="h-1/2 w-3/4">
                 <ThemeToggle></ThemeToggle>
-            <h1 className="h-3">Estou aqui</h1>
+                <h1 className="h-3">Estou aqui</h1>
 
             </div>
         </>
