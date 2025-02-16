e784568bde25b476d4784b80d2edbc4b91eb7b28
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-16 18:35:01 -0300

    começando base todo

--- src/app/components/todo.js
+++ src/app/components/todo.js
@@ -1,7 +1,19 @@
-const Todo = (props) =>{
-    return(<div className={`bg-yellow-300 w-80 h-40 dark:bg-white `}>
-       <h1>{props.title}</h1>
-       </div>
+import Image from "next/image"
+const Todo = (props) => {
+    return (
+        <div className={`flex max-w-96 h-48  border border-black `}>
+            <div className="w-3/4 border border-red">
+
+            </div>
+            <div className=" flex flex-col w-1/4 h-full justify-between border border-red">
+                <div className=" h-12 m-2 border border-black">
+                    <button  onClick={console.log("abaoba")} className="bg-fuchsia-300 h-full w-full">    
+                   
+                    </button>
+                </div>
+                <div className=" h-12 m-2 border border-black"> </div>
+            </div>
+        </div>
     )
-} 
+}
 export default Todo
