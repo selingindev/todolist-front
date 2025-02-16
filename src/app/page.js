9df80dde8dfc96883cc6ab32505780ce234708ff
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-15 21:42:22 -0300

    retirando fundo app

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
+
 import Page from "./Home/page";
 
 export default function App() {
   return (
-    <div class="flex h-auto w-auto min-h-svh bg-white">
+    <>
       <Page> </Page>
-    </div>
-
+    </>
   );
 }
