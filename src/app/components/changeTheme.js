53183faa686f31cd3679b41b470393a7c4331e27
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-16 16:17:16 -0300

    migrando useEffect

--- src/app/components/changeTheme.js
+++ src/app/components/changeTheme.js
@@ -1,11 +1,18 @@
+ 
 import { useContext } from "react";
 import { ThemeContext } from "../contexts/themeContext";
+import { useEffect  } from "react";
 
 const ThemeToggle = () => {
-  const {theme, setTheme }= useContext(ThemeContext)
+  const {theme, setTheme}= useContext(ThemeContext)
+  useEffect( () => {
+    document.querySelector('html').classList.remove('dark', 'light')
+    document.querySelector('html').classList.add(theme)
+},[theme]) 
+  
   return (
     
-    <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}  className={theme} class="p-2 rounded-full bg-light-card transition border border-dark-background">
+    <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} class="p-2 rounded-full bg-light-card transition border border-dark-background dark:border-red-600">
       <img
         src={theme === "light" ? "/assets/darkTheme.png" : "/assets/light.png"}
         alt={theme === "light" ? "Modo Escuro" : "Modo Claro"}
