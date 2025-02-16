38169456c620f39b7e887152f000c9a1d10d50c5
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-16 18:35:24 -0300

    mudança cores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,14 +13,14 @@ export default {
         foreground: "var(--foreground)",
         light: {
           background: '#F5F5F5',
-          card: '#FFFFFF',
+          card: '#1E1E1E',
           textPrimary: '#212121',
           textSecondary: '#757575',
           accent: '#007AFF',
         },
         dark: {
           background: '#121212',
-          card: '#1E1E1E',
+          card: '#FFFFFF',
           textPrimary: '#E0E0E0',
           textSecondary: '#B0B0B0',
           accent: '#4A90E2',
