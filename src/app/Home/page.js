c4d2a6f527adc35a6de344dd9b110b9e8f07abb7
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-16 13:46:11 -0300

    deletando local storage que não tinha função e ainda bugava a renderização

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -1,46 +1,27 @@
 'use client'
-import { useState, useEffect } from "react";
 import ThemeToggle from "../components/changeTheme";
 import Todo from "../components/todo";
-
+import { ThemeContext } from "../contexts/themeContext";
+import { useState } from "react";
 const Page = () => { 
-      // Estado para armazenar o tema atual, pegando do localStorage ou usando 'light' como padrão
-  const [theme, setTheme] = useState(() => {
-    if (typeof window !== 'undefined') {
-      return localStorage.getItem("theme") || "light";
-    }
-    return "light";
-  });
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      localStorage.setItem("theme", theme);
-      if (theme === "dark") {
-        document.body.classList.add("dark");
-      } else {
-        document.body.classList.remove("dark");
-      }
-    }
-  }, [theme]);
-
-  const changeValueTheme = () => {
-    setTheme (theme === "light" ? "dark" : "light");
-  };
-
+    const [theme, setTheme] = useState()
+    
     return (
+        <ThemeContext.Provider value={{theme, setTheme}}>
         <div  className={`flex h-auto w-auto min-h-svh ${theme === "light" ? "bg-light-background" : "bg-dark-background"}`}>
             {/* Construindo interface inicial */}
             <div className="h-1/2 w-1/4">
                 <div className=" w-full flex justify-end">
-                 <ThemeToggle changeTheme={changeValueTheme} theme={theme}></ThemeToggle>        
+                <ThemeToggle/>
                 </div>
             </div>
             <div className="h-1/2 w-3/4">
-        
                 <h1 className="h-3">Estou aqui</h1>
                 <Todo name="artu"></Todo>
             </div>
         </div>
+        </ThemeContext.Provider>
     );
 }
 
