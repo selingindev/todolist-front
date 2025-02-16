91c77631eb83da9330b437538c54889c63ee34c7
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-15 21:42:36 -0300

    deletando o codigo

--- src/app/components/changeTheme.js
+++ src/app/components/changeTheme.js
@@ -1,36 +1,10 @@
-'use client'
-import { useState, useEffect } from "react";
-
-const ThemeToggle = () => {
-  // Estado para armazenar o tema atual, pegando do localStorage ou usando 'light' como padrão
-  const [theme, setTheme] = useState(() => {
-    if (typeof window !== 'undefined') {
-      return localStorage.getItem("theme" || "light");
-    }
-    return "light";
-  });
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      localStorage.setItem("theme", theme);
-      if (theme === "dark") {
-        document.body.classList.add("dark");
-      } else {
-        document.body.classList.remove("dark");
-      }
-    }
-  }, [theme]);
-
-  const changeValueTheme = () => {
-    setTheme (theme === "light" ? "dark" : "light");
-  };
-
+const ThemeToggle = (props) => {
   return (
-    <button onClick={changeValueTheme} class="p-2 rounded-full bg-light-card transition border border-dark-background">
+    <button onClick={props.changeTheme} className="p-2 rounded-full bg-light-card transition border border-dark-background">
       <img
-        src={theme === "light" ? "/assets/darkTheme.png" : "/assets/light.png"}
-        alt={theme === "light" ? "Modo Escuro" : "Modo Claro"}
-        className={`w-6 h-6 p-0 ${theme === "light" ? "" : ""}`}
+        src={props.theme === "light" ? "/assets/darkTheme.png" : "/assets/light.png"}
+        alt={props.theme === "light" ? "Modo Escuro" : "Modo Claro"}
+        className={`w-6 h-6 p-0`}
 
       />
     </button>
