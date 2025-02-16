c30d815f46d576c39e311382d5732ad6aee18350
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-15 21:42:46 -0300

    começando interface todo

--- src/app/components/todo.js
+++ src/app/components/todo.js
@@ -1,5 +1,7 @@
-export default  todo = () =>{
-    <div>
-
+const Todo = (props) =>{
+    return(<div className={`bg-black w-100 h-100 `}>
+       <h1>{props.title}</h1> 
     </div>
+    )
 } 
+export default Todo
