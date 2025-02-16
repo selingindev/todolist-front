81c762c336578c1ed33965f2f037345d61ac5139
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-16 18:34:15 -0300

    arrumando rota

--- src/app/components/changeTheme.js
+++ src/app/components/changeTheme.js
@@ -14,7 +14,7 @@ const ThemeToggle = () => {
     
     <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} class="p-2 rounded-full bg-light-card transition border border-dark-background dark:border-red-600">
       <img
-        src={theme === "light" ? "/assets/darkTheme.png" : "/assets/light.png"}
+        src={theme === "light" ? "/darkTheme.png" : "/light.png"}
         alt={theme === "light" ? "Modo Escuro" : "Modo Claro"}
         className={`w-6 h-6 p-0`}
 
