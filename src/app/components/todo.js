18fe81a622da97f8d9326924e396b6b57769f2d4
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-19 11:35:10 -0300

    terminando visual botões todo

--- src/app/components/todo.js
+++ src/app/components/todo.js
@@ -1,28 +1,37 @@
 import { useContext } from "react";
-import { MdDelete } from "react-icons/md";
-import { MdEdit } from "react-icons/md";
 import { ThemeContext } from "../contexts/themeContext";
+import { MdDelete, MdDone, MdEdit } from "react-icons/md";
 
 const Todo = () => {
-    const {theme} = useContext(ThemeContext)
+    const { theme } = useContext(ThemeContext)
     return (
         <div className={`flex max-w-96 h-48  border border-black `}>
+            { /* lado esquerdo container:) */}
             <div className="w-3/4 border border-red">
 
             </div>
+
+            { /* lado direito container:) */}
             <div className=" flex flex-col w-1/4 h-full justify-between border border-red">
-                <div className=" flex flex-col h-12 m-2 ">
-                    
-                    <button  onClick={() => console.log()} className="flex justify-end
-                    ">    
-                        <MdDelete color="red" size={30} />
+
+                { /* container superior:) */}
+                <div className=" flex flex-col items-end h-auto w-auto m-2 ">
+                    <button onClick={() => console.log()} className="flex justify-end w-auto h-auto m-1
+                    ">
+                        <MdDelete color={theme === 'light' ? "#212121" : "#E0E0E0"} size={30} />
                     </button>
-                    <button  onClick={() => console.log()} className="flex justify-end 
-                    ">    
-                        <MdEdit color={theme === 'light' ? "black" : "blue" }  size={30}/>
+                    <button onClick={() => console.log()} className="flex justify-end w-auto h-auto m-1
+                    ">
+                        <MdEdit color={theme === 'light' ? "#212121" : "#E0E0E0"} size={30} />
                     </button>
                 </div>
-                <div className=" h-12 m-2 border border-black"> </div>
+
+                { /* container inferior:) */}
+                <div className="flex h-12 m-2 justify-end">
+                    <button onClick={() => console.log()}>
+                        <MdDone color={theme === 'light' ? "#212121" : "#E0E0E0"} size={40} />
+                    </button>
+                </div>
             </div>
         </div>
     )
