83c1b0126120b0d7893ead15c7d62419e1b3c0a8
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-26 16:59:16 -0300

    formatação

--- src/app/service/api/todoService.js
+++ src/app/service/api/todoService.js
@@ -14,35 +14,35 @@ export async function fetchTodos() {
     console.error('Erro na requisição:', error);
     return [];
   }
- 
+
 }
 
 export async function postTodo(data) {
   const urlData = await fetch('http://localhost:8080/todos');
- fetch(urlData, {
-  method: "POST",
-  headers: {
-    'Content-Type': 'application.json'
-  },
-  body: json.stringify({data})
-})
+  fetch(urlData, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application.json'
+    },
+    body: json.stringify({ data })
+  })
 }
 
 export async function editTodo(id, data) {
   await fetch(`http://localhost:8080/todos/${id}`, {
-   method: "PUT",
-   headers: {
-     'Content-Type': 'application.json'
-   },
-   body: json.stringify({id, data})
- })
- }
+    method: "PUT",
+    headers: {
+      'Content-Type': 'application.json'
+    },
+    body: json.stringify({ id, data })
+  })
+}
 
- export async function deleteTodo(id) {
+export async function deleteTodo(id) {
   const response = await fetch(`http://localhost:8080/todos/${id}`, {
     method: 'DELETE',
     headers: {
-        'Content-Type': 'application/json'
+      'Content-Type': 'application/json'
     }
   });
 
