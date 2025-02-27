3f4044634b8bbaab97761376831272bfb3873ba8
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-26 23:57:55 -0300

    criançao modal add todo

--- src/app/components/modalAddTodo.js
+++ src/app/components/modalAddTodo.js
@@ -1,6 +1,10 @@
+import FormTodo from "./formTodo";
+
 const modalAddTodo = () => {
     return(
-        <div className="bg-black size-4/5  dark:bg-fuchsia-400"> <button></button></div>
+        <div className=" w-80 h-96 absolute m-auto bg-dark-card dark:bg-light-card "  >
+            <FormTodo> </FormTodo>
+        </div>  
     )
 }
 export default modalAddTodo;
