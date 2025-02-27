0943efe7d246ce3e3483c6af96a1da56fd6bc6c9
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-26 23:57:38 -0300

    criando formTodo

---
+++ src/app/components/formTodo.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { postTodo } from "../service/api/todoService";
+
+const FormTodo = () => {
+    const [title, setTitle] = useState('');
+    const [desc, setDesc] = useState('');
+    const [prioridade, setPrioridade] = useState(0);
+
+    const heandleSubmit = async (e) => {
+        e.preventDefault();
+        if (title.trim() == "" || desc.trim() == "" || prioridade.trim() == "") return;
+
+        const data = {
+            title: title,
+            desc: desc,
+            prioridade: prioridade,
+            isDone: false
+        }
+        try {
+            console.log(data)
+            await postTodo(data);
+            setTitle("");
+            setDesc("");
+            setPrioridade("")
+        } catch (error) {
+            console.error("Erro ao adicionar tarefa:", error);
+        }
+    }
+
+    return (
+        <form onSubmit={heandleSubmit} className=" h-full w-full p-4 border border-cyan-700">
+            <div className="w-full h-3/4 flex flex-col justify-between">
+                <div className=" w-full h-1/3 py-4 ">
+                    <input type="text" value={title} placeholder="Digite o titulo da Tarefa" onChange={(e) => setTitle(e.target.value)} className="w-full h-full p-1" />
+                </div>
+                <div className=" w-full h-1/3 py-4 ">
+                    <input type="text" value={desc} placeholder="Digite a descrição da Tarefa" onChange={(e) => setDesc(e.target.value)} className="w-full h-full p-1" />
+                </div>
+                <div className=" w-full h-1/3 py-4 ">
+                    <input type="number" value={prioridade} onChange={(e) => setPrioridade(e.target.value)} placeholder="Selecione a prioridade da tarefa" className="outline w-full h-full p-1" />
+                </div>
+            </div>
+            <div className="w-full h-1/4">
+                <button onClick={() => {heandleSubmit }} className="w-full h-full bg-fuchsia-50">
+                
+                </button>
+            </div>
+        </form>
+    )
+}
+export default FormTodo;
