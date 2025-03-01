2638e6da4324a077520fc06948655a175d2923b5
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-28 22:26:12 -0300

    criação modal reutilizavel

---
+++ src/app/components/modal.js
@@ -0,0 +1,15 @@
+import { ModalContext } from "../contexts/Context";
+import { useContext,} from "react";
+
+const Modal = ({children } ) => {
+
+    const {modalOpen} = useContext(ModalContext);
+    
+    return (
+        
+        modalOpen ? children  : null 
+        
+    );
+    
+}
+export default Modal
---
+++ src/app/components/modalTodo.js
@@ -0,0 +1,11 @@
+import FormTodo from "./formTodo";
+
+const   ModalTodo = () => {
+    
+    return(
+        <div className=" w-80 h-96 absolute m-auto bg-dark-card dark:bg-light-card "  >
+            <FormTodo> </FormTodo>
+        </div>  
+    )
+}
+export default ModalTodo;
