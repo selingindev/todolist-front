31c7a1e49106c5e25d9c0264623bd1e744f120f7
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-28 22:26:36 -0300

    passando custom function

--- src/app/components/novaTarefaButton.js
+++ src/app/components/novaTarefaButton.js
@@ -2,37 +2,48 @@ import { useContext, useEffect, useState } from "react";
 import { IoAddCircleSharp } from "react-icons/io5";
 import { CiCircleRemove } from "react-icons/ci";
 
-import ModalAddTodo from "../components/modalAddTodo";
-import { ModalContext, ThemeContext } from "../contexts/Context";
+import ModalTodo from "../components/modalTodo";
+import { ContentModalContext, ModalContext, ThemeContext } from "../contexts/Context";
+
+import Modal from "./modal";
 
 
 const NovaTarefaButton = () => {
     const { modalOpen, setModalOpen } = useContext(ModalContext);
-    const { theme } = useContext(ThemeContext)
+    const { theme } = useContext(ThemeContext);
+    const {setCustomFunction} = useContext(ContentModalContext)
 
-   
-    const handleOpenModal = () => {
-        setModalOpen(true);
-    };
-
-    const handleCloseModal = () => {
-        setModalOpen(false);
-    };
-
+    
+    const callback = () => {
+        setCustomFunction("Create");
+      
+        if (modalOpen === false) {
+          setModalOpen(true);
+        } else {
+          setModalOpen(false);
+        }
+      };
+      
     return (
         <>
 
-            <button onClick={modalOpen == false ? handleOpenModal : handleCloseModal}>
+            <div 
+                onClick={ callback 
+                } 
+            
+                  className="cursor-pointer inline-flex items-center"
+            >
                 {
-                    modalOpen == false ? <IoAddCircleSharp size={40} color={theme == 'dark' ? 'white' : "black"} />
-                        : <CiCircleRemove size={40} color={theme == 'dark' ? 'white' : "black"} />
+                    modalOpen === false 
+                        ? <IoAddCircleSharp size={40} color={theme === 'dark' ? 'white' : "black"} />
+                        : <CiCircleRemove size={40} color={theme === 'dark' ? 'white' : "black"} />
                 }
-
-            </button>
-            {modalOpen == true ? <ModalAddTodo /> : null}
+            </div>
+            <Modal><ModalTodo /></Modal>
         </>
     );
 };
+
 
 export default NovaTarefaButton;
 
