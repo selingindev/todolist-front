9a5cfea70ac7790cca27d813a0d01a8aa2546a47
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-28 22:27:23 -0300

    Contexto para passar o id e o contexto da funçao

--- src/app/contexts/Context.js
+++ src/app/contexts/Context.js
@@ -4,3 +4,5 @@ const { createContext } = require("react");
 export const ThemeContext = createContext('light');
 export const ModalContext = createContext();
 export const TodoContext = createContext();
+export const ContentModalContext = createContext();
+export const IdTodoContext = createContext();
