cc6b618b87b437220f64893a93c47d97fa27782a
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-28 22:27:38 -0300

    passando novos providers

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -2,33 +2,39 @@
 import ThemeToggle from "../components/changeTheme";
 import NovaTarefaButton from "../components/novaTarefaButton";
 import Todo from "../components/todo";
-import { ModalContext, ThemeContext, TodoContext } from "../contexts/Context";
+import { ModalContext, ThemeContext, TodoContext, ContentModalContext, IdTodoContext } from "../contexts/Context";
 import { useState } from "react";
 const Page = () => {
   const [theme, setTheme] = useState('light')
   const [modalOpen, setModalOpen] = useState(false)
   const [todos, setTodos] = useState([]);
+  const [customFunction, setCustomFunction] = useState("Create")
+  const [id, setId] = useState(0)
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
-      <ModalContext.Provider value={{ modalOpen, setModalOpen }}>
-        <TodoContext.Provider value={{ todos, setTodos }}>
-          <div className={`flex h-auto w-auto min-h-svh dark:bg-dark-background bg-white "}`}>
-            {/* Construindo interface inicial */}
-            <div className="h-1/2 w-1/4">
-              <div className=" w-full flex justify-end">
-                <ThemeToggle />
+      <ContentModalContext.Provider value={{ customFunction, setCustomFunction }}>
+        <ModalContext.Provider value={{ modalOpen, setModalOpen }}>
+          <TodoContext.Provider value={{ todos, setTodos }}>
+            <IdTodoContext.Provider value={{ id, setId }}>
+              <div className={`flex h-auto w-auto min-h-svh dark:bg-dark-background bg-white "}`}>
+                {/* Construindo interface inicial */}
+                <div className="h-1/2 w-1/4">
+                  <div className=" w-full flex justify-end">
+                    <ThemeToggle />
+                  </div>
+                </div>
+                <div className="h-1/2 w-3/4">
+                  <NovaTarefaButton />
+
+                  <Todo />
+
+                </div>
               </div>
-            </div>
-            <div className="h-1/2 w-3/4">
-              <NovaTarefaButton />
-
-              <Todo />
-
-            </div>
-          </div>
-        </TodoContext.Provider>
-      </ModalContext.Provider>
+            </IdTodoContext.Provider>
+          </TodoContext.Provider>
+        </ModalContext.Provider>
+      </ContentModalContext.Provider>
     </ThemeContext.Provider>
   );
 }
