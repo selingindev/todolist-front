a5cea57a87b9a0d53eab1818c498a4002127d544
Arthur Selingin <arthur_santos@outlook.com.br>
2025-03-03 22:21:34 -0300

    usando get no todo para colocar os dados nos seus respectivos campos

--- src/app/components/formTodo.js
+++ src/app/components/formTodo.js
@@ -1,7 +1,6 @@
-import { useContext, useState } from "react";
-import { editTodo, postTodo } from "../service/api/todoService";
+import { useContext, useState,  useEffect} from "react";
+import { editTodo, getTodo, postTodo } from "../service/api/todoService";
 import { ContentModalContext, IdTodoContext, ModalContext, TodoContext } from "../contexts/Context";
-
 
 const FormTodo = () => {
     let { setModalOpen } = useContext(ModalContext);
@@ -12,22 +11,40 @@ const FormTodo = () => {
     const [desc, setDesc] = useState('')
     const [prioridade, setPrioridade] = useState(0)
 
-
     const handleCreate = async (data) => {
         const updateTodos = await postTodo(data);
         setTodos(updateTodos)
     };
+    
+   const dataTodo  = async () => {
+        if(customFunction === "Edit"){
+            const dataTodo = await getTodo(id);
+             setDesc(dataTodo.desc)
+             setPrioridade(dataTodo.prioridade)
+             setTitle(dataTodo.title)
+            }
+         return 
+        }
+    
+        useEffect(() => {
+            dataTodo(); 
+        }, []);
 
     const handleEdit = async (id, data) => {
         const updateTodos = await editTodo(id, data)
         setTodos(updateTodos)
     }
 
-
     const heandleSubmit = async (e) => {
 
         e.preventDefault();
-        if (title.trim() == "" || desc.trim() == "") return;
+        if (customFunction === "Create") {
+            if (title.trim() === "") {
+                alert("Impossível criar uma tarefa NULA")
+                return; 
+            }}
+        
+        
 
         let data = {
             id: null,
@@ -59,13 +76,13 @@ const FormTodo = () => {
         <form onSubmit={heandleSubmit} className=" h-full w-full p-4 border border-cyan-700">
             <div className="w-full h-3/4 flex flex-col justify-between">
                 <div className=" w-full h-1/3 py-4 ">
-                    <input type="text" value={title} placeholder="Digite o titulo da Tarefa" onChange={(e) => setTitle(e.target.value)} className="w-full h-full p-1" />
+                    <input type="text" value={title} placeholder="Digite o titulo da Tarefa" onChange={(e) => setTitle(e.target.value)} className="w-full h-full p-1 text-black" />
                 </div>
                 <div className=" w-full h-1/3 py-4 ">
-                    <input type="text" value={desc} placeholder="Digite a descrição da Tarefa" onChange={(e) => setDesc(e.target.value)} className="w-full h-full p-1" />
+                    <input type="text" value={desc} placeholder="Digite a descrição da Tarefa" onChange={(e) => setDesc(e.target.value)} className="w-full h-full p-1 text-black"  />
                 </div>
                 <div className=" w-full h-1/3 py-4 ">
-                    <input type="number" value={prioridade} onChange={(e) => setPrioridade(e.target.value)} placeholder="Selecione a prioridade da tarefa" className="outline w-full h-full p-1" />
+                    <input type="number" value={prioridade} onChange={(e) => setPrioridade(e.target.value)} placeholder="Selecione a prioridade da tarefa" className="outline w-full h-full p-1 text-black" />
                 </div>
             </div>
             <div className="w-full h-1/4">
