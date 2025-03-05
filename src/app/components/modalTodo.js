604ef677a5313edccd221bf6589f2908ae44ae43
Arthur Selingin <arthur_santos@outlook.com.br>
2025-03-04 22:23:08 -0300

    formatação

--- src/app/components/modalTodo.js
+++ src/app/components/modalTodo.js
@@ -1,11 +1,11 @@
 import FormTodo from "./formTodo";
 
-const   ModalTodo = () => {
-    
-    return(
+const ModalTodo = () => {
+
+    return (
         <div className=" w-80 h-96 absolute m-auto bg-dark-card dark:bg-light-card "  >
             <FormTodo> </FormTodo>
-        </div>  
+        </div>
     )
 }
 export default ModalTodo;
